fda9dabba90addde1bc0e780a5db4f33b2ac808c
jeremiahdickens <jeremiahdickens2@my.unt.edu>
2017-06-13 15:09:10 -0500

    Added all of the crop down boxes on dates, and added a submit button.

--- AS_Build/assets/js/firebase_getdates.js
+++ AS_Build/assets/js/firebase_getdates.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(user => {
     if (user != null) {
-      var name = user.displayName
+      var name = user.displayName;
       const dbRefObject = firebase.database().ref().child(name);
 
       dbRefObject.once('value', function(snapshot){
@@ -8,23 +8,25 @@ firebase.auth().onAuthStateChanged(user => {
           var childKey = childSnapshot.key;
 
           console.log("Dates Loaded: " + childKey);
-          var i = document.createElement("input");
+          /*var i = document.createElement("input");
           i.type = "checkbox";
-          i.value = childKey;
-          var text = document.createTextNode(" " + childKey);
-          var br = document.createElement('br');
+          i.value = childKey;*/
+
+
+//	  var text = document.createTextNode(" " + childKey);
+	  // drop down box for dates
+	  var dates_ddbox = document.getElementById("dates-ddbox");
+	  dates_ddbox.options[dates_ddbox.options.length] = new Option(childKey, childKey);
+	/*  var br = document.createElement('br');
           document.getElementById('dates').appendChild(i);
           document.getElementById('dates').appendChild(text);
-          document.getElementById('dates').appendChild(br);
+          document.getElementById('dates').appendChild(br);*/
 
       /*    const dbRefObject2 = firebase.database().ref().child(name).child(childKey);
           dbRefObject2.once('value', function(snapshot){
             snapshot.forEach(function(childSnapshot2){
               var flight = childSnapshot2.key;
 <<<<<<< HEAD
-
-
-
           console.log("Flight Number: " + flight);
           var p = document.createElement("input");
           p.type = "checkbox";
@@ -38,7 +40,7 @@ firebase.auth().onAuthStateChanged(user => {
           //document.getElementById('box').appendChild(style);
 =======
 */
-
+/*
 	  for(var i =0; i < childKey.length; i++)
 	  {
             var flightnum = "Flight " + (i+1);
@@ -58,7 +60,7 @@ firebase.auth().onAuthStateChanged(user => {
           	document.getElementById('dates').appendChild(text);
             document.getElementById('dates').appendChild(br);
         }
-
+*/
 	 //document.getElementById('dates').appendChild(br);
 
           /*var div = document.createElement("div");
@@ -98,26 +100,136 @@ m+1;
   console.log("The Date is: " + dt);
 }
 
-
 window.setInterval(function(){
-  fsearch();
-},500);
+  dateSearch();
+  UAVSearch();
+} ,500);
 
-function fsearch() {
-    var checked_vals = [];
-    var date;
-    $('#dates input:checkbox:checked').each(function(index) {
+function UAVSearch() {
+    //var checkedvalues = []
+      var user = firebase.auth().currentUser;
+      var dates_ddbox = document.getElementById("dates-ddbox");
+      var UAV_ddbox = document.getElementById("UAV-ddbox");
+      var flight_ddbox = document.getElementById("flight-ddbox");
+      if(user != null)
+      {
+          if(dates_ddbox.options.length > 0)
+          {
+              if(UAV_ddbox.options.length > 0)
+              {
+                    var name = user.displayName;
+          			    var date = dates_ddbox.options[dates_ddbox.selectedIndex].text;
+                    var UAVname = UAV_ddbox.options[UAV_ddbox.selectedIndex].text;
+                    const Flightref = firebase.database().ref(name + "/" + date + "/" + UAVname + "/");
+                    var currentOpt = false;
+                    if (UAVname != localStorage.UAVname || date != localStorage.date)
+                    {
+                          console.log( "TEST TO SEE IF THIS EVER HAPPENS");
+                          flight_ddbox.options.length = 0;
+                    }
+
+                    Flightref.once('value', function (snapshot){
+          				        snapshot.forEach(function(childSnapshot){
+          					             var childKey = childSnapshot.key;
+          					             console.log("Child key: " + childKey);
+
+                                 for (var i =0; i<flight_ddbox.options.length; i++)
+                                 {
+                                   if(flight_ddbox.options[i].value == childKey)
+                                   {
+                                     currentOpt = true;
+                                   }
+                                 }
+                                 if (!currentOpt)
+                                 {
+                                   flight_ddbox.options[flight_ddbox.options.length] = new Option(childKey, childKey);
+                                 }
+                           });
+                      });
+              }
+              localStorage.UAVname = UAVname;
+          }
+       	 	else
+       	 	{
+       	       		console.log("Still Loading...");
+       	 	}
+    }
+  }
+
+function dateSearch() {
+    //var checkedvalues = []
+      var user = firebase.auth().currentUser;
+      if(user != null)
+      {
+          var dates_ddbox = document.getElementById("dates-ddbox");
+          if(dates_ddbox.options.length > 0)
+          {
+              console.log("Text : " + dates_ddbox.options[dates_ddbox.selectedIndex].text +" Value : " + dates_ddbox.options[dates_ddbox.selectedIndex].value);
+
+
+                  var name = user.displayName;
+        			    var date = dates_ddbox.options[dates_ddbox.selectedIndex].text;
+                  const UAVref = firebase.database().ref(name + "/" + date + "/");
+                  var UAV_ddbox = document.getElementById("UAV-ddbox");
+                  var currentOpt = false;
+                  if (dates_ddbox.options[dates_ddbox.selectedIndex].text != localStorage.date)
+                  {
+                        console.log( "TEST TO SEE IF THIS EVER HAPPENS");
+                        UAV_ddbox.options.length = 0;
+                  }
+
+                  UAVref.once('value', function (snapshot){
+        				        snapshot.forEach(function(childSnapshot){
+        					             var childKey = childSnapshot.key;
+        					             console.log("Child key: " + childKey);
+
+                               var UAV_ddbox = document.getElementById("UAV-ddbox");
+                               for (var i =0; i<UAV_ddbox.options.length; i++)
+                               {
+                                 if(UAV_ddbox.options[i].value == childKey)
+                                 {
+                                   currentOpt = true;
+                                 }
+                               }
+                               if (!currentOpt)
+                               {
+                                 UAV_ddbox.options[UAV_ddbox.options.length] = new Option(childKey, childKey);
+                               }
+                         });
+                    });
+               localStorage.date = date;
+          }
+       	 	else
+       	 	{
+       	       		console.log("Still Loading...");
+                  return lastDate;
+       	 	}
+    }
+
+/*    $('#dates input:checkbox:checked').each(function(index) {
         checked_vals.push($(this).val());
-    });
+    });*/
     //console.log(checked_vals);
-    if (checked_vals.length === 1){
+   /* if (checked_vals.length === 1){
         date = checked_vals;
 
         //flight = checked_vals;
         localStorage.date = date;
         //localStorage.flight = flight;
-        window.location.href = "http://aeriumsolutions.com/map";
+        window.location.href = "http://aeriumsolutions.com/map";*/
         /*window.open("/map","_self");*/
-    }
+//    }
 
 }
+
+btnSubmit.addEventListener('click', e=> {
+  var dates_ddbox = document.getElementById("dates-ddbox");
+  var UAV_ddbox = document.getElementById("UAV-ddbox");
+  var flight_ddbox = document.getElementById("flight-ddbox");
+
+  localStorage.date = dates_ddbox.options[dates_ddbox.selectedIndex].text;
+  localStorage.UAVname = UAV_ddbox.options[UAV_ddbox.selectedIndex].text;
+  localStorage.flight = flight_ddbox.options[flight_ddbox.selectedIndex].text;
+  console.log("Date: " + localStorage.date + " UAV: " + localStorage.UAVname + " Flight: " + localStorage.flight);
+  window.location.href = "http://aeriumsolutions.com/map/";
+});
